098a34353cf150d8dedad961cd8b517bd799addd
Janilson Brito <jessica.costa.075@ufrn.edu.br>
2023-12-08 09:46:30 -0300

    l

--- backend1-main/src/main/java/com/example/demo/entities/Armazenamento.java
+++ backend1-main/src/main/java/com/example/demo/entities/Armazenamento.java
@@ -1,14 +1,19 @@
 package com.example.demo.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+import lombok.Getter;
+import lombok.Setter;
 import java.time.LocalDate;
 
+@Entity
+@Getter @Setter
 public class Armazenamento {
     
     @Id

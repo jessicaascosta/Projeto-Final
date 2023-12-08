21afcf05c1f7444d352a4eb4fd73ba50105312f9
Janilson Brito <jessica.costa.075@ufrn.edu.br>
2023-12-08 09:43:46 -0300

    i

--- backend1-main/src/main/java/com/example/demo/entities/Entrega.java
+++ backend1-main/src/main/java/com/example/demo/entities/Entrega.java
@@ -1,12 +1,17 @@
 package com.example.demo.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter @Setter
 public class Entrega {
 
     @Id

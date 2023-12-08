f15010159bc043cf19016eff3b07bbc93fc2b7ca
Janilson Brito <jessica.costa.075@ufrn.edu.br>
2023-12-08 09:35:41 -0300

    ok

--- backend1-main/src/main/java/com/example/demo/entities/Entrega.java
+++ backend1-main/src/main/java/com/example/demo/entities/Entrega.java
@@ -11,7 +11,7 @@ public class Entrega {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @Column(nullable = false)
     private String dataSaida;
